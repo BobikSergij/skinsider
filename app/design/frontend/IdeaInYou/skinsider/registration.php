80282be34b93974c04ca58d40a5ad787c2b12787
stanislav.o <stanislav.o@ideainyou.com>
2022-07-01 01:09:00 +0300

    Added Skinsider Theme and New Fonts

---
+++ app/design/frontend/IdeaInYou/skinsider/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * IdeaInYou_skinsider
+ *
+ * @copyright   Copyright (c) 2022 IdeaInYou Company (www.ideainyou.com)
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/IdeaInYou/skinsider', __DIR__);

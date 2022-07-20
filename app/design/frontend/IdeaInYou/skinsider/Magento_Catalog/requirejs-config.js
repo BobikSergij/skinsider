8b69031d5e2407b1f61efd04c06bc20679225eb5
stanislav.o <stanislav.o@ideainyou.com>
2022-07-20 03:47:46 +0300

    mobile functionality + some scripts

--- app/design/frontend/IdeaInYou/skinsider/Magento_Catalog/requirejs-config.js
+++ app/design/frontend/IdeaInYou/skinsider/Magento_Catalog/requirejs-config.js
@@ -1,7 +1,9 @@
 var config = {
     map: {
         "*": {
-            toggleCategoryDescription: 'Magento_Catalog/js/toggle-category-description'
+            toggleCategoryDescription: 'Magento_Catalog/js/toggle-category-description',
+            toggleCategoryToolbar: 'Magento_Catalog/js/toggle-category-toolbar',
+            toggleCategoryFilter: 'Magento_Catalog/js/toggle-category-filter'
         }
     }
 }

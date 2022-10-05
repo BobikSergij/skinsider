229bf29e60f6edbefedbe87fa0a34915abb304b9
Dim44ik7 <dmytro.p@ideainyou.com>
2022-10-05 15:34:26 +0300

    SK-70: Header fixed

--- app/design/frontend/IdeaInYou/skinsider/Magento_Theme/web/js/headerFixed.js
+++ app/design/frontend/IdeaInYou/skinsider/Magento_Theme/web/js/headerFixed.js
@@ -11,10 +11,10 @@ define([
             let $headerClass = $(this.element);
             $(window).scroll(function(){
                 const $scroll = $(window).scrollTop();
-                if ($scroll >= 300) {
+                if ($scroll >= 10) {
                     $headerClass.addClass('fixed');
                 }
-                else if($scroll <= 300) {
+                else if($scroll <= 100) {
                     $headerClass.removeClass('fixed');
                 }
             });
